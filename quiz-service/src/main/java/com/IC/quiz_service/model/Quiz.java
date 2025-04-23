ef77b94ce8a55876daff117aa6557fe96769734c
MohannadFathy0 <mohannadfathy123@gmail.com>
2025-04-23 07:09:07 +0200

    first commit

---
+++ quiz-service/src/main/java/com/IC/quiz_service/model/Quiz.java
@@ -0,0 +1,50 @@
+package com.IC.quiz_service.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+
+    @ElementCollection
+    private List<Integer> questionIds;
+
+    public Quiz(Integer id, String title, List<Integer> questionIds) {
+        this.id = id;
+        this.title = title;
+        this.questionIds = questionIds;
+    }
+
+    public Quiz() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Integer> getQuestionIds() {
+        return questionIds;
+    }
+
+    public void setQuestionIds(List<Integer> questionIds) {
+        this.questionIds = questionIds;
+    }
+}
